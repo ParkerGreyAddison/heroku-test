2f4a222d6be00eca4f2d87efbde2088e1ea7af25
ParkerGreyAddison <pargrey@gmail.com>
2018-09-16 23:18:14 -0700

    Trying to scrape to PostgreSQL

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app
+worker: python scrape_to_db.py
---
+++ scrape_to_db.py
@@ -0,0 +1,53 @@
+import os
+import psycopg2
+import requests
+import json
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode="require")
+
+cur = conn.cursor()
+
+### Scrape from a zipcode and add to existing table ###
+
+# Vons
+url = 'https://shop.vons.com/bin/safeway/login'
+base = 'https://shop.vons.com/bin/safeway/product/results'
+
+
+# Scrape terms
+ZIP = 92092
+KEY = 'promo'
+VALUE = 'popular-items-1750'
+
+# data = {'resourcePath': '/content/shop/vons/en/welcome/jcr:content/root/responsivegrid/column_control/par_0/two_column_zip_code_', 'zipcode': 92092}
+data = {'zipcode':ZIP}
+payload = {'key':KEY, 'value':VALUE}
+
+
+# Session start
+s = requests.Session()
+
+s.post(url, data=data)
+r = s.get(base, params=payload)
+
+results = r.json()['products']
+
+
+# Index by id
+data = {}
+for item in results:
+    data[item.pop('id')] = item
+
+
+# Add to table as json dumps
+to_add = json.dumps(data)
+
+# Finally insert zipcode into column 'zipcode' and json into column 'json'
+cur.execute("INSERT INTO scrapes (zipcode, json) VALUES (%s, %s)" % (ZIP, to_add))
+
+# Close connection
+conn.commit()
+cur.close()
+conn.close()
