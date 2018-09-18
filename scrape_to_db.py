9ff1ee0eb80dde495d984ac4cd75f1435515b985
ParkerGreyAddison <pargrey@gmail.com>
2018-09-17 17:46:00 -0700

    Should have looked at psycopg2 docs sooner.

--- scrape_to_db.py
+++ scrape_to_db.py
@@ -3,28 +3,19 @@ import psycopg2
 import requests
 import json
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
-conn = psycopg2.connect(DATABASE_URL, sslmode="require")
-
-cur = conn.cursor()
-
-### Scrape from a zipcode and add to existing table ###
+### Scraping from Vons ###
 
 # Vons
 url = 'https://shop.vons.com/bin/safeway/login'
 base = 'https://shop.vons.com/bin/safeway/product/results'
-
 
 # Scrape terms
 ZIP = 92092
 KEY = 'promo'
 VALUE = 'popular-items-1750'
 
-# data = {'resourcePath': '/content/shop/vons/en/welcome/jcr:content/root/responsivegrid/column_control/par_0/two_column_zip_code_', 'zipcode': 92092}
 data = {'zipcode':ZIP}
 payload = {'key':KEY, 'value':VALUE}
-
 
 # Session start
 s = requests.Session()
@@ -34,26 +25,31 @@ r = s.get(base, params=payload)
 
 results = r.json()['products']
 
-
 # Index by id
 data = {}
 for item in results:
     data[item.pop('id')] = item
 
+# Convert to json string
+data_json = json.dumps(data)
 
-# Add to table as json dumps
-to_add = json.dumps(data)
 
-#! Not quite ready yet!!!
-    # Need to wrap this string with single quotes, and replace all double quotes with two single quotes
-to_add.replace('"', "''")
-    # Wow that's a line of code I never want to see again
-final_json = "'" + to_add + "'"
+
+### Adding to table ###
+
+# Initializing connection and cursor
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode="require")
+cur = conn.cursor()
 
 # Finally insert zipcode into column 'zipcode' and json into column 'json'
-cur.execute("INSERT INTO scrapes (zipcode, json) VALUES (%s, %s)" % (ZIP, final_json))
+cur.execute("INSERT INTO scrapes VALUES (%s, %s);", (ZIP, data_json))
+    # This worked practically line for line when testing locally.
 
-# Close connection
+#! For the project, the relevant keys of the json will receive their own columns,
+    # this is just for testing purposes... not a good practice in this case!
+
+# Commit and close connection
 conn.commit()
 cur.close()
 conn.close()
