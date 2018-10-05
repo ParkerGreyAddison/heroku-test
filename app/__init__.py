82a47e03ea75b155f58b12224d721d561fad9e63
ParkerGreyAddison <pargrey@gmail.com>
2018-10-05 11:53:30 -0700

    Setup socketio with gunicorn and eventlet.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,6 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "Not very secretive."
-socketio = SocketIO(app)
+socketio = SocketIO(app, debug=True)
 
 from app import routes
